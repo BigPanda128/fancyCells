240e6cd69f757d86ef67b8491466d43315fe2196
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-20 00:29:25 -0400

    scafolding code deleted

--- differenceBuild.js
+++ differenceBuild.js
@@ -14,23 +14,7 @@ function difference(x, y) {
 	print(TANKS[x].name + " build date minus " + TANKS[y].name + " build date");
 	var tank1 = new Date(TANKS[x].built).getTime();
 	var tank2 = new Date(TANKS[y].built).getTime();
-	print(tank1);
-	print(tank2);
 	var result = Math.floor((tank1 - tank2) / -86400000);
-	//print(result);
-	return (result + " days apart");
+	return ("The " + TANKS[x].name + " and " + TANKS[y].name + " were built " + result + " days apart");
 }
-//print(new Date(TANKS[0].built).getTime());
 print(difference(2,4));
-
-/*
-print("pick one");
-
-x = readline();
-
-print("pick another to find a difference in built dates");
-
-y = readline();
-
-print(x,y);
-*/
