93a14156944fc34ad6d3c98ffb702c5137196dea
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-19 23:03:10 -0400

    reedited data set for the data functions

---
+++ tanks.js
@@ -0,0 +1,21 @@
+//Ryan Postma
+//CMP237
+//DATA entry
+//Tanks from Worl of Tanks
+//sorted by name, nation, tank type, speed in km/h, calibur of gun in MM
+//and damage
+
+//all 33 prem tanks from world of tanks game
+var TANKS = [
+{ name: "pz.s35", nation: "german", type: "medium", caliber: 47, built: "3/15/1936"},
+{ name: "t-15", nation: "german", type: "light", caliber: 37, built: "1/20/1940"},
+{ name: "t-25", nation: "german", type: "medium", caliber: 75, built: "12/1/1941"},
+{ name: "dicker max", nation: "german", type: "tdspg", caliber: 105, built: "7/1/1939"},
+{ name: "pz. 4s.", nation: "german", type: "medium", caliber: 75, built: "9/19/1943"},
+{ name: "panther/m10", nation: "german", type: "medium", caliber: 75, built: "8/25/1944"},
+{ name: "jgtig. 8,8cm", nation: "german", type: "tdspg", caliber: 88, built: "2/4/1944"},
+{ name: "lowe", nation: "german", type: "heavy", caliber: 105, built: "6/21/1941"},
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = HOUSES;
